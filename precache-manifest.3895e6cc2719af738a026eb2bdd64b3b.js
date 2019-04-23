86dee952ff1f8a74c1b123db8d16c2fb2573d386
Hamilton Saxion <hsaxion8@gmail.com>
2019-04-22 20:16:57 -0500

    Updates

---
+++ precache-manifest.3895e6cc2719af738a026eb2bdd64b3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a7924b9bd0ed3376fe345065422cc46",
+    "url": "/Clicky-Game-GOT/static/media/GoT2.4a7924b9.png"
+  },
+  {
+    "revision": "18fad280124c3c1a5b2495a99cdf3305",
+    "url": "/Clicky-Game-GOT/static/media/GoT.18fad280.png"
+  },
+  {
+    "revision": "a9844fc52ab4dd1040fd",
+    "url": "/Clicky-Game-GOT/static/js/runtime~main.f189aeed.js"
+  },
+  {
+    "revision": "a4e75f8cc44bea56e8e4",
+    "url": "/Clicky-Game-GOT/static/js/main.42f2c40b.chunk.js"
+  },
+  {
+    "revision": "6badddc0ac41fe0662c9",
+    "url": "/Clicky-Game-GOT/static/js/2.c0fc9f2d.chunk.js"
+  },
+  {
+    "revision": "a4e75f8cc44bea56e8e4",
+    "url": "/Clicky-Game-GOT/static/css/main.65064241.chunk.css"
+  },
+  {
+    "revision": "a1363d16fc430ae5ab435878fac727da",
+    "url": "/Clicky-Game-GOT/index.html"
+  }
+];
